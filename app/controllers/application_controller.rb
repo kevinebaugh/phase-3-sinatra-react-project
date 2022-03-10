dcb014a0add61a8f923f73ce461c5f6a7013dc5e
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-09 21:55:02 -0500

    Add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,51 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    # Redirect to front-end
   end
 
+  # start GETs
+
+  get "/dogs" do
+    Dog.all.to_json
+  end
+
+  get "/waterbowls" do
+    Waterbowl.all.to_json
+  end
+
+  get "/waterbowl_visits" do
+    WaterbowlVisit.all.to_json
+  end
+
+  # end GETs
+
+  # start POSTs
+
+  post "/dogs" do
+    dog = Dog.create(
+      name: params[:name]
+    )
+    dog.to_json
+  end
+
+  post "/waterbowls" do
+    waterbowl= Waterbowl.create(
+      label: params[:label],
+      latitude: params[:latitude],
+      longitude: params[:longitude]
+    )
+    waterbowl.to_json
+  end
+
+  post "/waterbowl_visits" do
+    waterbowl_visit= WaterbowlVisit.create(
+      dog_id: params[:dog_id],
+      waterbowl_id: params[:waterbowl_id],
+      comment: params[:comment]
+    )
+    waterbowl_visit.to_json
+  end
+
+  # end POSTs
 end
