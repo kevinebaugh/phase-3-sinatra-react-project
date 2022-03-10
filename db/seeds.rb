58b2b22703f40eeb3efca6d2235bab0ab8d6a94b
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-03-09 21:23:49 -0500

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+Dog.create(name: "Rawley")
+puts "Dog created"
+
+Waterbowl.create(
+  label: "La Festa",
+  latitude: "43.1941631",
+  longitude: "-70.8770038"
+)
+puts "Waterbowl created"
+
+WaterbowlVisit.create(
+  dog_id: Dog.first.id,
+  waterbowl_id: Waterbowl.first.id,
+  comment: "Refreshing!"
+)
+puts "WaterbowlVisit created"
 
 puts "✅ Done seeding!"
